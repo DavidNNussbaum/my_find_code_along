8899fe4f1185b2ee0d9417c34230d54b5aba2840
DavidNNussbaum <brocknynj@aol.com>
2020-10-29 11:26:48 -0400

    commit tree 934661e7a6a7e38eba98dae3a076c6fc8c76b7b1

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,26 @@
-require 'pry'
+# require 'pry'
+# binding.pry
+# def my_find(collection)
+#     i = 0
+#     while i < collection.length
+#       i = i + 1
+#     end
+#   end 
+
+# def my_find(collection)
+#     i = 0
+#     while i < collection.length
+#     # binding.pry
+#       yield(collection[i])
+#       i = i + 1
+#     end
+#   end 
+
 
 def my_find(collection)
-
-end
+    i = 0
+    while i < collection.length
+      return collection[i] if yield(collection[i])
+      i = i + 1
+    end
+  end 
